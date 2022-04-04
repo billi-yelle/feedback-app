6dece938a3cde97bee60734d1c907fda85544517
billi-yelle <billi@mushroomblvd.com>
2022-04-04 15:59:14 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { useState } from 'react';
+import FeedbackList from './components/FeedbackList';
+import Header from './components/Header';
+import FeedbackData from './data/FeedbackData';
+
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = id => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter(item => item.id !== id));
+    }
+  };
+
   return (
-    <div className='container'>
-      <h1>My App</h1>
-      <p>Hello</p>
-    </div>
+    <>
+      <Header />
+      <div className='container'>
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
+    </>
   );
 }
 
